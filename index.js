794e9bf86c1b5ffc54db438e3f9a4ac2fa9f7171
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-05-18 21:28:42 +0600

    error solve

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
      try {
           // Connect the client to the server	(optional starting in v4.7)
-          await client.connect();
+          // await client.connect();
 
           // server link start
           const serverCollection = client.db('dbAssignment11').collection('cltAssignment11');
